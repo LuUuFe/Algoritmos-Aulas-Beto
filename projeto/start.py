fcb47b48138ff22a7c3ce070018636a62017c43d
LuUuFe <lufe8803@gmail.com>
2025-01-08 00:49:52 -0300

    Adicionando funcionalidades de filtragem, alocação e consulta dos dados!

--- projeto/start.py
+++ projeto/start.py
@@ -1,7 +1,78 @@
-students = []
-teachers = []
-disciplines = []
-classes = []
+students = [
+  {
+    'name': 'João Silva',
+    'registration': 'S001',
+    'dateOfBirth': '10/05/2005',
+    'gender': 'Masculino',
+    'address': 'Rua A, 123',
+    'phone': '1111-2222',
+    'email': 'joao.silva@email.com'
+  },
+  {
+    'name': 'Maria Oliveira',
+    'registration': 'S002',
+    'dateOfBirth': '15/08/2006',
+    'gender': 'Feminino',
+    'address': 'Rua B, 456',
+    'phone': '3333-4444',
+    'email': 'maria.oliveira@email.com'
+  }
+]
+
+teachers = [
+  {
+    'name': 'Carlos Pereira',
+    'registration': 'T001',
+    'dateOfBirth': '20/03/1980',
+    'gender': 'Masculino',
+    'address': 'Avenida X, 789',
+    'phone': '5555-6666',
+    'email': 'carlos.pereira@email.com',
+    'discipline': 'Matemática'
+  },
+  {
+    'name': 'Ana Souza',
+    'registration': 'T002',
+    'dateOfBirth': '12/07/1985',
+    'gender': 'Feminino',
+    'address': 'Avenida Y, 101',
+    'phone': '7777-8888',
+    'email': 'ana.souza@email.com',
+    'discipline': 'Português'
+  }
+]
+
+disciplines = [
+  {
+    'name': 'Matemática',
+    'code': 'D001',
+    'workload': '80 horas',
+    'teacher': 'Carlos Pereira'
+  },
+  {
+    'name': 'Português',
+    'code': 'D002',
+    'workload': '60 horas',
+    'teacher': 'Ana Souza'
+  }
+]
+
+classes = [
+  {
+    'name': 'Turma A',
+    'code': 'C001',
+    'discipline': 'Matemática',
+    'teacher': 'Carlos Pereira',
+    'students': ['S001', 'S002']
+  },
+  {
+    'name': 'Turma B',
+    'code': 'C002',
+    'discipline': 'Português',
+    'teacher': 'Ana Souza',
+    'students': ['S001']
+  }
+]
 
 # Passando para informar que toda essa parte onde eu coleto os dados para cadastrar eu pedir para o ChatGPT terminar para mim!
 
@@ -53,6 +124,66 @@ def register_class():
   classes.append(classInfo)
   print("Turma cadastrada com sucesso!\n")
 
+def filter_teachers_per_discipline():
+  print([discipline['name'] for discipline in disciplines])
+  disciplineSearch = input("Digite o nome da disciplina: ")
+  teachersPerDiscipline = list(filter(lambda teacher: teacher['discipline'] == disciplineSearch, teachers))
+  print([teacher['name'] for teacher in teachersPerDiscipline])
+
+def enroll_student_in_class():
+  studentRegistration = input("Digite a matrícula do aluno: ")
+  classCode = input("Digite o código da turma: ")
+  classFound = next((cls for cls in classes if cls['code'] == classCode), None)
+  if classFound:
+    classFound['students'].append(studentRegistration)
+    print("Aluno matriculado com sucesso!\n")
+  else:
+    print("Turma não encontrada!\n")
+
+def allocate_teacher_to_discipline():
+  teacher_name = input("Digite o nome do professor: ")
+  disciplineName = input("Digite o nome da disciplina: ")
+  disciplineFound = next((discipline for discipline in disciplines if discipline['name'] == disciplineName), None)
+  if disciplineFound:
+    disciplineFound['teacher'] = teacher_name
+    print("Professor alocado com sucesso!\n")
+  else:
+    print("Disciplina não encontrada!\n")
+
+def allocate_discipline_to_class():
+  disciplineName = input("Digite o nome da disciplina: ")
+  classCode = input("Digite o código da turma: ")
+  classFound = next((cls for cls in classes if cls['code'] == classCode), None)
+  if classFound:
+    classFound['discipline'] = disciplineName
+    print("Disciplina alocada com sucesso!\n")
+  else:
+    print("Turma não encontrada!\n")
+
+def consult_students_in_class():
+  classCode = input("Digite o código da turma: ")
+  classFound = next((cls for cls in classes if cls['code'] == classCode), None)
+  if classFound:
+    print("Alunos matriculados:", classFound['students'])
+  else:
+    print("Turma não encontrada!\n")
+
+def consult_teachers_in_discipline():
+  disciplineName = input("Digite o nome da disciplina: ")
+  disciplineFound = next((discipline for discipline in disciplines if discipline['name'] == disciplineName), None)
+  if disciplineFound:
+    print("Professor alocado:", disciplineFound['teacher'])
+  else:
+    print("Disciplina não encontrada!\n")
+
+def consult_disciplines_in_class():
+  classCode = input("Digite o código da turma: ")
+  classFound = next((cls for cls in classes if cls['code'] == classCode), None)
+  if classFound:
+    print("Disciplina alocada:", classFound['discipline'])
+  else:
+    print("Turma não encontrada!\n")
+
 def show_menu():
   while True:
     print("\nSistema Escolar")
@@ -60,7 +191,14 @@ def show_menu():
     print("2. Cadastrar Professor")
     print("3. Cadastrar Disciplina")
     print("4. Cadastrar Turma")
-    print("5. Sair")
+    print("5. Filtrar Professores Por Disciplina")
+    print("6. Matricular Aluno em Turma")
+    print("7. Alocar Professor em Disciplina")
+    print("8. Alocar Disciplina em Turma")
+    print("9. Consultar Alunos Matriculados em Turma")
+    print("10. Consultar Professores Alocados em Disciplina")
+    print("11. Consultar Disciplinas Alocadas em Turma")
+    print("0. Sair")
     
     choice = input("Escolha uma opção: ")
 
@@ -73,9 +211,22 @@ def show_menu():
     elif choice == '4':
       register_class()
     elif choice == '5':
+      filter_teachers_per_discipline()
+    elif choice == '6':
+      enroll_student_in_class()
+    elif choice == '7':
+      allocate_teacher_to_discipline()
+    elif choice == '8':
+      allocate_discipline_to_class()
+    elif choice == '9':
+      consult_students_in_class()
+    elif choice == '10':
+      consult_teachers_in_discipline()
+    elif choice == '11':
+      consult_disciplines_in_class()
+    elif choice == '0':
       print("Saindo do sistema. Até logo!")
       break
     else:
       print("Opção inválida. Tente novamente.\n")
-
 show_menu()
